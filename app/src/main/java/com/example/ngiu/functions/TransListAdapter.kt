6ec3f43091d2d24dc00f2f57e4865fcf56e3df9e
roboterz <40050902+roboterz@users.noreply.github.com>
2021-10-05 01:21:04 -0400

    add transaction list view and load data

--- app/src/main/java/com/example/ngiu/functions/TransListAdapter.kt
+++ app/src/main/java/com/example/ngiu/functions/TransListAdapter.kt
@@ -1,15 +1,26 @@
 package com.example.ngiu.functions
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getColor
+import androidx.core.graphics.toColor
+import androidx.core.graphics.toColorLong
 import androidx.recyclerview.widget.RecyclerView
 import androidx.room.TypeConverters
 import com.example.ngiu.R
 import com.example.ngiu.data.entities.DateTypeConverter
 import com.example.ngiu.data.entities.Trans
+import com.google.android.material.color.MaterialColors.getColor
+import com.google.android.material.internal.ContextUtils.getActivity
 import kotlinx.android.synthetic.main.transaction_cardview.view.*
+import kotlinx.coroutines.currentCoroutineContext
 import java.util.*
 
 
@@ -29,37 +40,47 @@ class TransListAdapter(private val trans: MutableList<Trans>)
     }
 
 
+    @SuppressLint("ResourceAsColor")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // display the custom class
         trans[position].apply {
-            holder.day.text = "$ID"
-            //holder.week.text =""
-            //holder.dailyIncome.text =""
-            //holder.dailyExpense.text =""
+            holder.day.text = Date.month.toString()
+            holder.week.text = Date.month.toString()
+            holder.income.text = "Income"
+            holder.dailyIncome.text ="$0.00"
+            holder.expense.text = "Expense"
+            holder.dailyExpense.text ="$0.00"
             holder.subCategory.text ="$SubCategoryID"
-            //holder.time.text =""
+            holder.time.text = trans[position].Date.toString()
             holder.period.text ="$PeriodID"
             holder.memo.text ="$Memo"
             holder.payer.text ="$PayerID"
             holder.person.text ="$PersonID"
             holder.merchant.text ="$MerchantID"
             holder.project.text ="$ProjectID"
-            holder.amount.text ="$Amount"
+            holder.amount.text ="$$Amount"
 
-            /*
-            if (holder.ID.text.toString().toLong() > 10) {
-                holder.ID.setTextColor(-0xffff01)
+            if (holder.person.text !=""){
+                holder.beforePerson.text =" • "
             }
-            if (holder.ID.text.toString().toLong() > 20) {
-                holder.ID.setTextColor(-0xff5501)
+            if (holder.merchant.text !=""){
+                holder.beforeMerchant.text =" • "
             }
-            if (holder.ID.text.toString().toLong() > 30) {
-                holder.ID.setTextColor(-0xff0101)
+            if (holder.project.text !=""){
+                holder.beforeProject.text =" • "
             }
 
-             */
+            when (TransTypeID.toInt()) {
+                1 -> holder.amount.setTextColor(Color.RED)
+                2 -> holder.amount.setTextColor(Color.GREEN)
+                else -> holder.amount.setTextColor(Color.BLACK)
+            }
+
+
+
         }
     }
+
 
 
     override fun getItemCount(): Int {
@@ -71,6 +92,8 @@ class TransListAdapter(private val trans: MutableList<Trans>)
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val day: TextView = itemView.tvTrans_day
         val week: TextView = itemView.tvTrans_week
+        val expense: TextView = itemView.tvTrans_daily_expense
+        val income: TextView = itemView.tvTrans_daily_income
         val dailyExpense: TextView = itemView.tvTrans_daily_expense_amount
         val dailyIncome: TextView = itemView.tvTrans_daily_income_amount
         val subCategory: TextView = itemView.tvTrans_category
@@ -82,6 +105,9 @@ class TransListAdapter(private val trans: MutableList<Trans>)
         val person: TextView = itemView.tvTrans_person
         val project: TextView = itemView.tvTrans_project
         val amount: TextView = itemView.tvTrans_amount
+        val beforePerson: TextView = itemView.tvTrans_dot_before_person
+        val beforeMerchant: TextView = itemView.tvTrans_dot_before_merchant
+        val beforeProject: TextView = itemView.tvTrans_dot_before_project
     }
 
 
