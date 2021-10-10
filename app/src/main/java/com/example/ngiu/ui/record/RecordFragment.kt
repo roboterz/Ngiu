d2d1e386569e3a4cd3401a823d01cc66febd545f
roboterz <40050902+roboterz@users.noreply.github.com>
2021-10-10 02:18:34 -0400

    touch transaction type direct to the target page

--- app/src/main/java/com/example/ngiu/ui/record/RecordFragment.kt
+++ app/src/main/java/com/example/ngiu/ui/record/RecordFragment.kt
@@ -1,33 +1,17 @@
 package com.example.ngiu.ui.record
 
-import android.app.Instrumentation
-import android.content.ComponentCallbacks
-import android.content.Context
+
 import android.os.Bundle
-import android.view.KeyEvent
-import android.view.KeyEvent.KEYCODE_BACK
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
-import android.widget.Toolbar
-import androidx.activity.ComponentActivity
-import androidx.activity.addCallback
-import androidx.appcompat.widget.ActionBarContextView
-import androidx.core.view.marginLeft
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainer
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
 import com.example.ngiu.MainActivity
 import com.example.ngiu.R
 import com.example.ngiu.databinding.FragmentRecordBinding
-import com.example.ngiu.functions.MyFunctions
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_activity.*
 import kotlinx.android.synthetic.main.fragment_record.*
 
 
@@ -53,20 +37,39 @@ class RecordFragment : Fragment() {
         _binding = FragmentRecordBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        // focus on Expense page when open up
+        //setStatus( recordViewModel.chooseTransactionType(1) )
+
 
         // todo load record data
         //Toast.makeText(context,"open",Toast.LENGTH_SHORT).show()
-
 
         return root
     }
 
 
-
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Keep current state when reloading
+        setStatus(recordViewModel.optionChoice)
+
+        // touch Expense textView, switch to Expense page
+        tvSectionExpense.setOnClickListener {
+            setStatus(recordViewModel.chooseTransactionType(1))
+        }
+        // touch Income textView, switch to Income page
+        tvSectionIncome.setOnClickListener {
+            setStatus(recordViewModel.chooseTransactionType(2))
+        }
+        // touch Transfer textView, switch to Transfer page
+        tvSectionTransfer.setOnClickListener {
+            setStatus(recordViewModel.chooseTransactionType(3))
+        }
+        // touch DebitCredit textView, switch to DebitCredit page
+        tvSectionDebitCredit.setOnClickListener {
+            setStatus(recordViewModel.chooseTransactionType(4))
+        }
 
 
         // set up toolbar icon and click event
@@ -103,11 +106,25 @@ class RecordFragment : Fragment() {
 
     }
 
+    private fun setStatus(optionChoice: OptionChoice){
+        tvSectionExpense.setTextColor(ContextCompat.getColor(requireContext(),optionChoice.expense))
+        tvSectionExpensePointer.visibility = optionChoice.expensePointer
+        tvSectionIncome.setTextColor(ContextCompat.getColor(requireContext(),optionChoice.income))
+        tvSectionIncomePointer.visibility = optionChoice.incomePointer
+        tvSectionTransfer.setTextColor(ContextCompat.getColor(requireContext(),optionChoice.transfer))
+        tvSectionTransferPointer.visibility = optionChoice.transferPointer
+        tvSectionDebitCredit.setTextColor(ContextCompat.getColor(requireContext(),optionChoice.debitCredit))
+        tvSectionDebitCreditPointer.visibility = optionChoice.debitCreditPointer
+    }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         (activity as MainActivity).setNavBottomBarVisibility(View.VISIBLE)
         _binding = null
     }
+
+
 }
 
--- app/src/main/java/com/example/ngiu/ui/record/RecordViewModel.kt
+++ app/src/main/java/com/example/ngiu/ui/record/RecordViewModel.kt
@@ -1,8 +1,11 @@
 package com.example.ngiu.ui.record
 
+
+import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.ngiu.R
 
 class RecordViewModel : ViewModel() {
 
@@ -10,4 +13,56 @@ class RecordViewModel : ViewModel() {
         value = "This is notifications Fragment"
     }
     val text: LiveData<String> = _text
+
+    var optionChoice: OptionChoice = OptionChoice()
+
+
+
+    fun chooseTransactionType(ttid: Int): OptionChoice{
+        val activeText = R.color.white
+        val activePointer = View.VISIBLE
+
+        optionChoice = OptionChoice().clearPreset()
+
+        when (ttid) {
+            1 -> {
+                optionChoice.expense = activeText
+                optionChoice.expensePointer = activePointer
+            }
+            2 -> {
+                optionChoice.income = activeText
+                optionChoice.incomePointer = activePointer
+            }
+            3 -> {
+                optionChoice.transfer = activeText
+                optionChoice.transferPointer = activePointer
+            }
+
+            4 -> {
+                optionChoice.debitCredit = activeText
+                optionChoice.debitCreditPointer = activePointer
+            }
+        }
+        return optionChoice
+    }
+}
+
+class OptionChoice{
+    var expense:  Int = R.color.white
+    var expensePointer: Int = View.VISIBLE
+    var income:  Int = R.color.color_inactive_text
+    var incomePointer:  Int = View.INVISIBLE
+    var transfer:  Int = R.color.color_inactive_text
+    var transferPointer:  Int = View.INVISIBLE
+    var debitCredit:  Int = R.color.color_inactive_text
+    var debitCreditPointer:  Int = View.INVISIBLE
+
+    fun clearPreset(): OptionChoice{
+        val optionChoice = OptionChoice()
+
+        optionChoice.expense = R.color.color_inactive_text
+        optionChoice.expensePointer = View.INVISIBLE
+
+        return optionChoice
+    }
 }
