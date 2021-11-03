2166f00481cff9823a529e835d89dc68a9d26b44
nystins <70663896+nystins@users.noreply.github.com>
2021-11-03 19:33:20 -0400

    update add UI

--- app/src/main/java/com/example/ngiu/ui/account/addaccounts/AddCashFragment.kt
+++ app/src/main/java/com/example/ngiu/ui/account/addaccounts/AddCashFragment.kt
@@ -1,26 +1,31 @@
 package com.example.ngiu.ui.account.addaccounts
 
+import android.app.AlertDialog
 import android.os.Bundle
-import android.provider.Settings
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.example.ngiu.R
 import com.example.ngiu.data.entities.Account
-import com.example.ngiu.data.entities.AccountType
+import com.example.ngiu.data.entities.Currency
 import com.example.ngiu.databinding.FragmentAddCashBinding
-import kotlinx.coroutines.Dispatchers
+import com.example.ngiu.functions.addDecimalLimiter
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.android.synthetic.main.fragment_record.*
+import kotlinx.android.synthetic.main.popup_title.view.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 
 class AddCashFragment : Fragment() {
-
-    private lateinit var binding: FragmentAddCashBinding
-
+    private var _binding: FragmentAddCashBinding? = null
+    private val binding get() = _binding!!
+    var currency = "USD"
 
     private val viewModel: AddCashViewModel by lazy {
         ViewModelProvider(this).get(AddCashViewModel::class.java)
@@ -30,14 +35,28 @@ class AddCashFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentAddCashBinding.inflate(inflater, container, false)
-        val view = binding.root
-        initListeners()
+        return FragmentAddCashBinding.inflate(inflater, container, false).run {
+            _binding = this
+            root
+        }
 
-
-        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+        getView()?.findViewById<TextInputEditText?>(R.id.tetCashBalance)?.addDecimalLimiter()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 
     private fun initListeners() {
         binding.btnSaveCash.setOnClickListener {
@@ -45,17 +64,55 @@ class AddCashFragment : Fragment() {
             val balance = binding.tetCashBalance.text.toString()
             val countInNetAsset: Boolean = binding.scCashCountNetAssets.isChecked
             val memo = binding.tetCashMemo.text.toString()
+            val accountTypeID = 1L
 
-            val account: Account = Account(Account_Name =  accountName, Account_Balance =  balance.toDouble(),
-                Account_CountInNetAssets =  countInNetAsset, Account_Memo = memo, AccountType_ID = 2, Currency_ID = "USD")
+            val cashAccount = Account(
+                Account_Name =  accountName, Account_Balance =  balance.toDouble(),
+                Account_CountInNetAssets =  countInNetAsset, Account_Memo = memo, AccountType_ID = accountTypeID, Currency_ID = currency)
 
-            GlobalScope.launch {
-                viewModel.insertData(requireActivity(), account)
+            GlobalScope.launch{
+                viewModel.insertCash(requireActivity(), cashAccount)
+            }
+        }
+
+        binding.btnCashAddOtherCurrency.setOnClickListener {
+
+            val array : List<Currency> = viewModel.getCurrency(requireActivity())
+
+            val arrayList: ArrayList<String> = ArrayList()
+
+            for (item in array){
+                arrayList.add(item.Currency_ID)
             }
 
+            val cs: Array<CharSequence> = arrayList.toArray(arrayOfNulls<CharSequence>(arrayList.size))
 
+            //   val array = arrayof()
+            // Initialize a new instance of alert dialog builder object
+            val builder = AlertDialog.Builder(requireContext())
+
+            // set Title Style
+            val titleView = layoutInflater.inflate(R.layout.popup_title,null)
+            // set Title Text
+            titleView.tv_popup_title_text.text = getText(R.string.option_title_add_currency)
+
+            builder.setCustomTitle(titleView)
+
+            // Set items form alert dialog
+            builder.setItems(cs) { _, which ->
+                // Get the dialog selected item
+                val selected = array[which]
+                currency = arrayList.get(which)
+                Toast.makeText(context, "You Clicked: " + selected, Toast.LENGTH_SHORT).show()
+            }
+
+            // Create a new AlertDialog using builder object
+            // Finally, display the alert dialog
+            builder.create().show()
         }
+
     }
+
 
 
 }
--- app/src/main/java/com/example/ngiu/ui/account/addaccounts/AddDebitFragment.kt
+++ app/src/main/java/com/example/ngiu/ui/account/addaccounts/AddDebitFragment.kt
@@ -1,28 +1,118 @@
 package com.example.ngiu.ui.account.addaccounts
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.example.ngiu.R
+import com.example.ngiu.data.entities.Account
+import com.example.ngiu.data.entities.Currency
+import com.example.ngiu.databinding.FragmentAddCashBinding
+import com.example.ngiu.databinding.FragmentAddDebitBinding
+import com.example.ngiu.functions.addDecimalLimiter
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.android.synthetic.main.popup_title.view.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 
 class AddDebitFragment : Fragment() {
+    private var _binding: FragmentAddDebitBinding? = null
+    private val binding get() = _binding!!
+    var currency = "USD"
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    private val viewModel: AddDebitViewModel by lazy {
+        ViewModelProvider(this).get(AddDebitViewModel::class.java)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_debit, container, false)
+        return FragmentAddDebitBinding.inflate(inflater, container, false).run {
+            _binding = this
+            root
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+        getView()?.findViewById<TextInputEditText?>(R.id.tetDebitBalance)?.addDecimalLimiter()
+    }
+
+    override fun onResume() {
+        super.onResume()
     }
 
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+
+    private fun initListeners() {
+        binding.btnSaveDebit.setOnClickListener {
+            val accountName = binding.tetDebitAccountName.text.toString()
+            val cardNumber = binding.tetDebitCardNumber.text.toString()
+            val balance = binding.tetDebitBalance.text.toString()
+            val countInNetAsset: Boolean = binding.scDebitCountNetAssets.isChecked
+            val memo = binding.tetDebitMemo.text.toString()
+            val accountTypeID = 2L
+
+            val debitAccount = Account(
+                Account_Name = accountName, Account_CardNumber = cardNumber,
+                Account_Balance = balance.toDouble(), Account_CountInNetAssets = countInNetAsset,
+                Account_Memo = memo, AccountType_ID = accountTypeID, Currency_ID = currency
+            )
+            GlobalScope.launch {
+                viewModel.insertDebit(requireActivity(), debitAccount)
+            }
+
+
+        }
+
+        binding.btnDebitAddOtherCurrency.setOnClickListener {
+
+            val array: List<Currency> = viewModel.getCurrency(requireActivity())
+
+            val arrayList: ArrayList<String> = ArrayList()
+
+            for (item in array) {
+                arrayList.add(item.Currency_ID)
+            }
+
+            val cs: Array<CharSequence> =
+                arrayList.toArray(arrayOfNulls<CharSequence>(arrayList.size))
+
+
+            // Initialize a new instance of alert dialog builder object
+            val builder = AlertDialog.Builder(requireContext())
+
+            // set Title Style
+            val titleView = layoutInflater.inflate(R.layout.popup_title, null)
+            // set Title Text
+            titleView.tv_popup_title_text.text = getText(R.string.option_title_add_currency)
+
+            builder.setCustomTitle(titleView)
+
+            // Set items form alert dialog
+            builder.setItems(cs) { _, which ->
+                // Get the dialog selected item
+                val selected = array[which]
+                currency = arrayList.get(which)
+                Toast.makeText(context, "You Clicked: " + selected, Toast.LENGTH_SHORT).show()
+            }
+
+            // Create a new AlertDialog using builder object
+            // Finally, display the alert dialog
+            builder.create().show()
+        }
+
+    }
 }
