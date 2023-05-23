d48f1b9af0f9d2cd1679f7098c9905bb6f853027
roboterz <40050902+roboterz@users.noreply.github.com>
2023-05-22 23:14:24 -0400

    adjust transaction cardview

--- app/src/main/java/com/example/ngiu/ui/activity/ActivityListAdapter.kt
+++ app/src/main/java/com/example/ngiu/ui/activity/ActivityListAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.ngiu.ui.activity
 
 import android.annotation.SuppressLint
+import android.provider.Settings.Global.getString
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,10 @@ import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ngiu.R
 import com.example.ngiu.data.entities.returntype.TransactionDetail
+import com.example.ngiu.functions.TRANSACTION_TYPE_DEBIT
+import com.example.ngiu.functions.TRANSACTION_TYPE_EXPENSE
+import com.example.ngiu.functions.TRANSACTION_TYPE_INCOME
+import com.example.ngiu.functions.TRANSACTION_TYPE_TRANSFER
 import kotlinx.android.synthetic.main.cardview_transaction.view.*
 import java.time.format.DateTimeFormatter
 import kotlin.collections.ArrayList
@@ -20,9 +25,8 @@ class ActivityListAdapter(
     )
     : RecyclerView.Adapter<ActivityListAdapter.ViewHolder>() {
 
+    //
     private var transDetail: List<TransactionDetail> = ArrayList()
-
-    private val formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm")
 
     // interface for passing the onClick event to fragment.
     interface OnClickListener {
@@ -46,62 +50,41 @@ class ActivityListAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // display the custom class
         transDetail[position].apply {
+            // Date
             holder.monthDay.text = Transaction_Date.format(DateTimeFormatter.ofPattern("MM/dd"))
-            holder.week.text =Transaction_Date.format(DateTimeFormatter.ofPattern("EEEE"))
-            //holder.income.text = "Income"
-            //holder.dailyIncome.text ="$0.00"
-            //holder.expense.text = "Expense"
-            //holder.dailyExpense.text ="$0.00"
-            holder.time.text = Transaction_Date.format(DateTimeFormatter.ofPattern("HH:mm"))
-            holder.year.text = Transaction_Date.format(DateTimeFormatter.ofPattern("yyyy"))
-            //holder.period.text = ""
-            holder.memo.text = Transaction_Memo
+
+            // year and week
+            holder.yearWeek.text = Transaction_Date.format(DateTimeFormatter.ofPattern("yyyy EEEE"))
+
+            // second line
+            holder.secondLine.text = Transaction_Date.format(DateTimeFormatter.ofPattern("HH:mm"))
+            if (Period_ID > 0L) holder.secondLine.text = holder.secondLine.text.toString() + "    🔁"
+            if (Transaction_Memo.isNotEmpty()) holder.secondLine.text = holder.secondLine.text.toString() + "    $Transaction_Memo"
 
 
+            when (TransactionType_ID){
+                TRANSACTION_TYPE_EXPENSE, TRANSACTION_TYPE_INCOME ->{
+
+                    // first line
+                    holder.subCategory.text = Category_Name
+
+                    // third line
+                    holder.thirdLine.text = Account_Name
+                    if (Person_ID > 0L) holder.thirdLine.text = holder.thirdLine.text.toString() + " • $Person_Name"
+                    if (Merchant_ID > 1L) holder.thirdLine.text = holder.thirdLine.text.toString() + " • $Merchant_Name"
+                    if (Project_ID > 0L) holder.thirdLine.text = holder.thirdLine.text.toString() + " • $Project_Name"
+                }
+                TRANSACTION_TYPE_TRANSFER, TRANSACTION_TYPE_DEBIT ->{
+                    // first line
+                    holder.subCategory.text = "$Account_Name ➡️ $AccountRecipient_Name"
+                    // third line
+                    holder.thirdLine.visibility = View.GONE
+                }
+            }
+
+            // Amount
             holder.amount.text ="" + "%.2f".format(Transaction_Amount)
-
-            // TransactionType 1, 2
-            if (TransactionType_ID in 1L..2L) {
-                holder.subCategory.text = Category_Name
-                holder.account.text = Account_Name
-                holder.person.text = Person_Name
-                holder.merchant.text = Merchant_Name
-                holder.project.text = Project_Name
-                if (holder.person.text !=""){
-                    holder.beforePerson.text =" • "
-                }
-                if (holder.merchant.text !=""){
-                    holder.beforeMerchant.text =" • "
-                }
-                if (holder.project.text !=""){
-                    holder.beforeProject.text =" • "
-                }
-            }else{
-                holder.account.visibility = View.GONE
-                holder.person.visibility = View.GONE
-                holder.merchant.visibility = View.GONE
-                holder.project.visibility = View.GONE
-                holder.beforeMerchant.visibility = View.GONE
-                holder.beforePerson.visibility = View.GONE
-                holder.beforeProject.visibility = View.GONE
-            }
-
-
-
-            // Transaction Type 3, 4
-            if (TransactionType_ID in 3L..4L ){
-                holder.subCategory.text = Account_Name
-                holder.accountReceiver.text =" ➡️ $AccountRecipient_Name"
-                holder.accountReceiver.visibility = View.VISIBLE
-            } else {
-                holder.accountReceiver.text = ""
-            }
-
-            if (Period_ID != 0L) {
-                //holder.period.text="Period"
-                holder.period.visibility = View.VISIBLE
-            }
-
+            // Amount Color
             holder.amount.setTextColor(
                 when (TransactionType_ID.toInt()) {
                     1 -> holder.expenseColor
@@ -134,25 +117,12 @@ class ActivityListAdapter(
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val monthDay: TextView = itemView.tvTrans_month_day
-        val week: TextView = itemView.tvTrans_week
-        //val expense: TextView = itemView.tvTrans_daily_expense
-        //val income: TextView = itemView.tvTrans_daily_income
-        //val dailyExpense: TextView = itemView.tvTrans_daily_expense_amount
-        //val dailyIncome: TextView = itemView.tvTrans_daily_income_amount
-        val year: TextView = itemView.tvTrans_year
+        //val dailyInfo: TextView = itemView.tvTrans_daily_info
+        val yearWeek: TextView = itemView.tvTrans_year_week
         val subCategory: TextView = itemView.tvTrans_category
-        val time: TextView = itemView.tvTrans_time
-        val account: TextView = itemView.tvTrans_account_pay
-        val accountReceiver: TextView = itemView.tvTrans_account_receive
-        val merchant: TextView = itemView.tvTrans_merchant
-        val period: TextView = itemView.tvTrans_period
-        val memo: TextView = itemView.tvTrans_memo
-        val person: TextView = itemView.tvTrans_person
-        val project: TextView = itemView.tvTrans_project
+        val secondLine: TextView = itemView.tvTrans_second_line
+        val thirdLine: TextView = itemView.tvTrans_third_line
         val amount: TextView = itemView.tvTrans_amount
-        val beforePerson: TextView = itemView.tvTrans_dot_before_person
-        val beforeMerchant: TextView = itemView.tvTrans_dot_before_merchant
-        val beforeProject: TextView = itemView.tvTrans_dot_before_project
         val aItem: ConstraintLayout = itemView.layout_transaction_item
 
         val expenseColor = ContextCompat.getColor(itemView.context, R.color.app_expense_amount)
